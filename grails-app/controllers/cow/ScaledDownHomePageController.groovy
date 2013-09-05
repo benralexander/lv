f452bddeb16f5e9054082290541e8a026481ce70
Ben R Alexander <balexand@broadinstitute.org>
2013-09-05 10:02:41 +0100

    build out the scaled-down version of the Bard homepage in preparation for the ACS meeting

---
+++ grails-app/controllers/cow/ScaledDownHomePageController.groovy
@@ -0,0 +1,21 @@
+package cow
+
+class ScaledDownHomePageController {
+
+    def index() {
+        render(view: 'ScaledDownHomePage')
+    }
+    def bardtext_aboutBard() {
+        render(view: 'bardtext_aboutBard')
+    }
+    def bardtext_developmentTeam() {
+        render(view: 'bardtext_developmentTeam')
+    }
+    def bardtext_history() {
+        render(view: 'bardtext_history')
+    }
+    def bardtext_rdm() {
+        render(view: 'bardtext_rdm')
+    }
+    def ScaledDownHomePage() { }
+}
