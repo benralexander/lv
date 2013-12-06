b2fd980112372c38e4e5ac5cdea96695b8069cf5
Ben R. Alexander <balexand@broadinstitute.org>
2013-12-06 14:47:24 -0500

    load up files necessary to support jqgri GQ gridd

---
+++ grails-app/controllers/cow/JqgridController.groovy
@@ -0,0 +1,75 @@
+package cow
+
+class JqgridController {
+
+    def index() {
+//        String stringRequestedPage = params.page ?: "1" // get the requested page
+//        String stringRowsWeWant = params.rows ?: "1" // get how many rows we want to have into the grid
+//        String stringIndexRow = params.sidx ?: "1"// get index row - i.e. user click to sort
+//        String stringDirection = params.sord ?: "1" // get the direction
+//        Integer requestedPage, rowsWeWant, indexRow, direction
+//        try  {
+//            requestedPage = Integer.parseInt(stringRequestedPage)
+//            rowsWeWant = Integer.parseInt(stringRowsWeWant)
+//            indexRow = Integer.parseInt(stringIndexRow)
+//            direction = Integer.parseInt(stringDirection)
+//        }  catch (Exception exception) {
+//            println "whoops – bad parameter"
+//        }
+        render(view: 'jqgrid')
+    }
+    def jqgrid() { }
+    def feedMeJson(){
+        String stringRequestedPage = params.page ?: "1" // get the requested page
+        String stringRowsWeWant = params.rows ?: "1" // get how many rows we want to have into the grid
+        String indexRow = params.sidx ?: "id"// get index row name
+        String direction = params.sord ?: "desc" // get the direction
+        Integer requestedPage, rowsWeWant
+        try  {
+            requestedPage = Integer.parseInt(stringRequestedPage)
+            rowsWeWant = Integer.parseInt(stringRowsWeWant)
+        }  catch (Exception exception) {
+            println "whoops – bad parameter"
+        }
+        if (requestedPage==1){
+            render("""
+{
+  "total": "20",
+  "page": "1",
+  "records": "10",
+  "rows" :[
+{"id":"1","invdate":"2007-10-01","name":"test","note":"note","amount":"200.00","tax":"10.00","total":"210.00"},
+{"id":"2","invdate":"2007-10-02","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
+{"id":"3","invdate":"2007-09-01","name":"test3","note":"note3","amount":"400.00","tax":"30.00","total":"430.00"},
+{"id":"4","invdate":"2007-10-04","name":"test","note":"note","amount":"200.00","tax":"10.00","total":"210.00"},
+{"id":"5","invdate":"2007-10-05","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
+{"id":"6","invdate":"2007-09-06","name":"test3","note":"note3","amount":"400.00","tax":"30.00","total":"430.00"},
+{"id":"7","invdate":"2007-10-04","name":"test","note":"note","amount":"200.00","tax":"10.00","total":"210.00"},
+{"id":"8","invdate":"2007-10-03","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
+{"id":"9","invdate":"2007-10-05","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
+{"id":"10","invdate":"2007-09-06","name":"test3","note":"note3","amount":"400.00","tax":"30.00","total":"430.00"}]
+}""".toString())
+
+        } else {
+        render("""
+{
+  "total": "20",
+  "page": "1",
+  "records": "10",
+  "rows" :[
+{"id":"11","invdate":"2007-10-01","name":"test","note":"note","amount":"200.00","tax":"10.00","total":"210.00"},
+{"id":"12","invdate":"2007-10-02","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
+{"id":"13","invdate":"2007-09-01","name":"test3","note":"note3","amount":"400.00","tax":"30.00","total":"430.00"},
+{"id":"14","invdate":"2007-10-04","name":"test","note":"note","amount":"200.00","tax":"10.00","total":"210.00"},
+{"id":"15","invdate":"2007-10-05","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
+{"id":"16","invdate":"2007-09-06","name":"test3","note":"note3","amount":"400.00","tax":"30.00","total":"430.00"},
+{"id":"17","invdate":"2007-10-04","name":"test","note":"note","amount":"200.00","tax":"10.00","total":"210.00"},
+{"id":"18","invdate":"2007-10-03","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
+{"id":"19","invdate":"2007-10-05","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
+{"id":"20","invdate":"2007-09-06","name":"test3","note":"note3","amount":"400.00","tax":"30.00","total":"430.00"}
+]
+}""".toString())
+    }
+    }
+
+}
---
+++ grails-app/views/jqgrid/jqgrid.gsp
@@ -0,0 +1,69 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ben
+  Date: 12/6/13
+  Time: 10:35 AM
+--%>
+
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+
+
+    <title>jqgrid</title>
+    <script src="../js/bardHomepage/jquery-1.8.3.min.js"></script>
+    <script src="../js/jqgrid/jquery.jqGrid.src.js"></script>
+    <script src="../js/jqgrid/grid.locale-en.js"></script>
+    <link rel="stylesheet" type="text/css" href="${resource(dir: 'css', file: 'ui.jqgrid.css')}" />
+    <link rel="stylesheet" type="text/css" href="${resource(dir: 'css', file: 'jquery-ui.css')}" />
+
+    <style>
+    body{
+        margin: 0 auto;
+        max-width: 1400px;
+    }
+    </style>
+    <script>
+        function A(){
+            $("#list2").jqGrid({
+                url:'feedMeJson?q=2',
+                datatype: "json",
+                colNames:['ID','Date', 'Client','note', 'Amount','Tax','Total'],
+                colModel:[
+                    {name:'id',index:'id', width:55},
+                    {name:'invdate',index:'invdate', width:130},
+                    {name:'name',index:'name asc, invdate', width:100},
+                    {name:'note',index:'note', width:150, sortable:false},
+                    {name:'amount',index:'amount', width:80, align:"right"},
+                    {name:'tax',index:'tax', width:80, align:"right"},
+                    {name:'total',index:'total', width:80,align:"right"}
+                ],
+                rowNum:10,
+                rowList:[5,10],
+                pager: '#pager2',
+                sortname: 'id',
+                viewrecords: true,
+                sortorder: "desc",
+                caption:"JSON Example"
+            });
+            $("#list2").jqGrid('navGrid','#pager2',{edit:false,add:false,del:false});
+        }
+    </script>
+</head>
+
+<body>
+<h1> Oh boy, here is my new table!</h1>
+<table id="list2"></table>
+<div id="pager2"></div>
+<script>
+    console.log('a');
+A();
+    console.log('b');
+</script>
+
+<h3> There it went – I hope that you saw it</h3>
+
+
+
+</body>
+</html>
