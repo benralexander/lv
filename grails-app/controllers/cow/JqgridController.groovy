155ecfdf44fbb5450919e062bc05ae6d0a8120b8
Ben R Alexander <balexand@broadinstitute.org>
2013-12-09 19:57:11 +0000

    server-side scrolling and sorting

--- grails-app/controllers/cow/JqgridController.groovy
+++ grails-app/controllers/cow/JqgridController.groovy
@@ -1,27 +1,67 @@
 package cow
 
+import junit.framework.Assert
+
 class JqgridController {
+    Random random = new Random()
+    static int TOTAL_NUMBER_SIMULATED_RECORDS = 2000
+
+
+    String rn (int smallest,int biggest) {
+        int rawRandom = random.nextInt(biggest-smallest)
+        if (smallest > biggest) {
+            println "smallest > biggest.  What were you thinking?"
+            Assert  (smallest <= biggest)
+        }
+        int rawWithRangeRestriction =  (rawRandom%(biggest-smallest)) +  smallest
+        return  rawWithRangeRestriction.toString()
+    }
+
+
+    String simulatedRows(int firstIndex,
+                         int requestedPage,
+                         int numberRowsRequested) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int indexCounter = firstIndex
+        for (int i = 0; i < numberRowsRequested; i++) {
+            if ((firstIndex+i) < TOTAL_NUMBER_SIMULATED_RECORDS) {
+                String temporaryDate = "${rn(2000, 2012)}-${rn(1, 12)}-${rn(1, 12)}"
+                stringBuilder << "{\"id\":\"${indexCounter++}\",\"invdate\":\"${temporaryDate}\",\"name\":\"test${rn(0, 9)}\",\"note\":\"note${rn(0, 3)}\",\"amount\":\"${rn(1, 400)}.${rn(0, 99)}\",\"tax\":\"${rn(1, 10)}.00\",\"total\":\"210.00\"}"
+                if ((i + 1) < numberRowsRequested) {
+                    stringBuilder << ","
+                }
+            }
+        }
+        return stringBuilder.toString()
+    }
+
+    String simulatedRecord(int requestedPage,
+                           int numberRowsRequested){
+        //// "${TOTAL_NUMBER_SIMULATED_RECORDS/numberRowsRequested}"
+        String returnValue = """
+{
+  "total": "66",
+  "page": "${requestedPage}",
+  "records": "${TOTAL_NUMBER_SIMULATED_RECORDS}",
+  "rows" :[
+""".toString()
+        returnValue +=  simulatedRows( ((requestedPage*numberRowsRequested)-numberRowsRequested)+1,
+                 requestedPage,
+                 numberRowsRequested )
+        returnValue +=  """]
+    }""".toString()
+        return returnValue
+    }
+
+
 
     def index() {
-//        String stringRequestedPage = params.page ?: "1" // get the requested page
-//        String stringRowsWeWant = params.rows ?: "1" // get how many rows we want to have into the grid
-//        String stringIndexRow = params.sidx ?: "1"// get index row - i.e. user click to sort
-//        String stringDirection = params.sord ?: "1" // get the direction
-//        Integer requestedPage, rowsWeWant, indexRow, direction
-//        try  {
-//            requestedPage = Integer.parseInt(stringRequestedPage)
-//            rowsWeWant = Integer.parseInt(stringRowsWeWant)
-//            indexRow = Integer.parseInt(stringIndexRow)
-//            direction = Integer.parseInt(stringDirection)
-//        }  catch (Exception exception) {
-//            println "whoops – bad parameter"
-//        }
         render(view: 'jqgrid')
     }
     def jqgrid() { }
     def feedMeJson(){
         String stringRequestedPage = params.page ?: "1" // get the requested page
-        String stringRowsWeWant = params.rows ?: "1" // get how many rows we want to have into the grid
+        String stringRowsWeWant = params.rows ?: "1" // get how many rows we want to have in the grid
         String indexRow = params.sidx ?: "id"// get index row name
         String direction = params.sord ?: "desc" // get the direction
         Integer requestedPage, rowsWeWant
@@ -31,45 +71,47 @@ class JqgridController {
         }  catch (Exception exception) {
             println "whoops – bad parameter"
         }
-        if (requestedPage==1){
-            render("""
-{
-  "total": "20",
-  "page": "1",
-  "records": "10",
-  "rows" :[
-{"id":"1","invdate":"2007-10-01","name":"test","note":"note","amount":"200.00","tax":"10.00","total":"210.00"},
-{"id":"2","invdate":"2007-10-02","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
-{"id":"3","invdate":"2007-09-01","name":"test3","note":"note3","amount":"400.00","tax":"30.00","total":"430.00"},
-{"id":"4","invdate":"2007-10-04","name":"test","note":"note","amount":"200.00","tax":"10.00","total":"210.00"},
-{"id":"5","invdate":"2007-10-05","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
-{"id":"6","invdate":"2007-09-06","name":"test3","note":"note3","amount":"400.00","tax":"30.00","total":"430.00"},
-{"id":"7","invdate":"2007-10-04","name":"test","note":"note","amount":"200.00","tax":"10.00","total":"210.00"},
-{"id":"8","invdate":"2007-10-03","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
-{"id":"9","invdate":"2007-10-05","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
-{"id":"10","invdate":"2007-09-06","name":"test3","note":"note3","amount":"400.00","tax":"30.00","total":"430.00"}]
-}""".toString())
-
-        } else {
-        render("""
-{
-  "total": "20",
-  "page": "2",
-  "records": "10",
-  "rows" :[
-{"id":"11","invdate":"2007-10-01","name":"test","note":"note","amount":"200.00","tax":"10.00","total":"210.00"},
-{"id":"12","invdate":"2007-10-02","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
-{"id":"13","invdate":"2007-09-01","name":"test3","note":"note3","amount":"400.00","tax":"30.00","total":"430.00"},
-{"id":"14","invdate":"2007-10-04","name":"test","note":"note","amount":"200.00","tax":"10.00","total":"210.00"},
-{"id":"15","invdate":"2007-10-05","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
-{"id":"16","invdate":"2007-09-06","name":"test3","note":"note3","amount":"400.00","tax":"30.00","total":"430.00"},
-{"id":"17","invdate":"2007-10-04","name":"test","note":"note","amount":"200.00","tax":"10.00","total":"210.00"},
-{"id":"18","invdate":"2007-10-03","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
-{"id":"19","invdate":"2007-10-05","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
-{"id":"20","invdate":"2007-09-06","name":"test3","note":"note3","amount":"400.00","tax":"30.00","total":"430.00"}
-]
-}""".toString())
-    }
+        String proposedJson = simulatedRecord( requestedPage,rowsWeWant)
+        render  proposedJson
+//        if (requestedPage==1){
+//            render("""
+//{
+//  "total": "20",
+//  "page": "1",
+//  "records": "10",
+//  "rows" :[
+//{"id":"1","invdate":"2007-10-01","name":"test","note":"note","amount":"200.00","tax":"10.00","total":"210.00"},
+//{"id":"2","invdate":"2007-10-02","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
+//{"id":"3","invdate":"2007-09-01","name":"test3","note":"note3","amount":"400.00","tax":"30.00","total":"430.00"},
+//{"id":"4","invdate":"2007-10-04","name":"test","note":"note","amount":"200.00","tax":"10.00","total":"210.00"},
+//{"id":"5","invdate":"2007-10-05","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
+//{"id":"6","invdate":"2007-09-06","name":"test3","note":"note3","amount":"400.00","tax":"30.00","total":"430.00"},
+//{"id":"7","invdate":"2007-10-04","name":"test","note":"note","amount":"200.00","tax":"10.00","total":"210.00"},
+//{"id":"8","invdate":"2007-10-03","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
+//{"id":"9","invdate":"2007-10-05","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
+//{"id":"10","invdate":"2007-09-06","name":"test3","note":"note3","amount":"400.00","tax":"30.00","total":"430.00"}]
+//}""".toString())
+//
+//        } else {
+//        render("""
+//{
+//  "total": "20",
+//  "page": "2",
+//  "records": "10",
+//  "rows" :[
+//{"id":"11","invdate":"2007-10-01","name":"test","note":"note","amount":"200.00","tax":"10.00","total":"210.00"},
+//{"id":"12","invdate":"2007-10-02","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
+//{"id":"13","invdate":"2007-09-01","name":"test3","note":"note3","amount":"400.00","tax":"30.00","total":"430.00"},
+//{"id":"14","invdate":"2007-10-04","name":"test","note":"note","amount":"200.00","tax":"10.00","total":"210.00"},
+//{"id":"15","invdate":"2007-10-05","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
+//{"id":"16","invdate":"2007-09-06","name":"test3","note":"note3","amount":"400.00","tax":"30.00","total":"430.00"},
+//{"id":"17","invdate":"2007-10-04","name":"test","note":"note","amount":"200.00","tax":"10.00","total":"210.00"},
+//{"id":"18","invdate":"2007-10-03","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
+//{"id":"19","invdate":"2007-10-05","name":"test2","note":"note2","amount":"300.00","tax":"20.00","total":"320.00"},
+//{"id":"20","invdate":"2007-09-06","name":"test3","note":"note3","amount":"400.00","tax":"30.00","total":"430.00"}
+//]
+//}""".toString())
+//    }
     }
 
 }
--- grails-app/views/jqgrid/jqgrid.gsp
+++ grails-app/views/jqgrid/jqgrid.gsp
@@ -14,6 +14,7 @@
     <script src="../js/bardHomepage/jquery-1.8.3.min.js"></script>
     <script src="../js/jqgrid/jquery.jqGrid.src.js"></script>
     <script src="../js/jqgrid/grid.locale-en.js"></script>
+    <script src="../js/jqgrid/jquery-ui-1.10.3.custom.min.js"></script>
     <link rel="stylesheet" type="text/css" href="${resource(dir: 'css', file: 'ui.jqgrid.css')}" />
     <link rel="stylesheet" type="text/css" href="${resource(dir: 'css', file: 'jquery-ui.css')}" />
 
@@ -24,7 +25,7 @@
     /*}*/
     </style>
     <script>
-        function A(){
+                function A(){
             $("#list2").jqGrid({
                 url:'feedMeJson',
                 datatype: "json",
@@ -53,6 +54,114 @@
             });
             $("#list2").jqGrid('navGrid','#pager2',{edit:false,add:false,del:false});
         }
+
+        function AA(){
+            $("#list2").jqGrid({
+                url:'feedMeJson',
+                datatype: "json",
+                height: 255,
+                width: 600,
+                colNames:['ID','Date', 'Client','note', 'Amount','Tax','Total'],
+                colModel:[
+                    {name:'id',index:'id', width:55, sorttype:"int"},
+                    {name:'invdate',index:'invdate', width:130, sorttype:"date"},
+                    {name:'name',index:'name asc', width:100, sorttype:"string"},
+                    {name:'note',index:'note', width:150, sortable:false},
+                    {name:'amount',index:'amount', width:80, align:"right", sorttype:"float"},
+                    {name:'tax',index:'tax', width:80, align:"right", sorttype:"float"},
+                    {name:'total',index:'total', width:80,align:"right", sorttype:"float"}
+                ],
+                rowNum: 21,
+                scroll:1,
+                //rowTotal:2000,
+                jsonReader: {
+                    repeatitems : true,
+                    cell:"",
+                    id: "0"
+                },
+                scroll: 1,
+                loadonce:true,
+                mtype: "GET",
+                rownumbers: true,
+                rownumWidth: 40,
+                 pager: '#pager2',
+                sortname: 'id',
+                viewrecords: true,
+                sortorder: "asc",
+                virtualscroll:"true",
+                caption:""
+            });
+            //$("#list2").jqGrid('navGrid','#pager2',{del:false,add:false,edit:false},{},{},{},{multipleSearch:true});
+            $("#list2").jqGrid('navGrid','#pager2',  // Turn on the icons
+                    {edit:true,
+                        add:true,
+                        del:true,
+                        search:true,
+                        refresh:true,
+                        refreshstate:'current',
+                        view:true
+                    },
+                    // Edit dialog parameters
+                    {reloadAfterSubmit: false,
+                        closeAfterEdit: true
+                    },
+                    // Add dialog parameters
+                    {reloadAfterSubmit: true,
+                        closeAfterAdd: true
+                    },
+                    // Delete dialog parameters
+                    {reloadAfterSubmit: false},
+                    // Search dialog parameters
+                    {},
+                    // View dialog parameters
+                    {});
+        }
+
+
+                function AAA(){
+                    $("#list2").jqGrid({
+                        width: 600,
+                        hoverrows: false,
+                        viewrecords: true,
+                        "jsonReader":{
+                            "repeatitems":false,
+                            "subgrid":
+                            {"repeatitems":false}
+                        },
+                        "xmlReader":{"repeatitems":false,"subgrid":{"repeatitems":false}},
+                        gridview: true,
+                        url:'feedMeJson',
+                        datatype: "json",
+                        scroll: 1,
+                        rowNum:50,
+                        sortname: 'id',
+                        height: 255,
+                        colNames:['id','Date', 'Client','note', 'Amount','Tax','Total'],
+                        colModel:[
+                            {name:'id',index:'id', width:55},
+                            {name:'invdate',index:'invdate', width:130},
+                            {name:'name',index:'name asc, invdate', width:100},
+                            {name:'note',index:'note', width:150, sortable:false},
+                            {name:'amount',index:'amount', width:80, align:"right"},
+                            {name:'tax',index:'tax', width:80, align:"right"},
+                            {name:'total',index:'total', width:80,align:"right"}
+                        ],
+                        "postData":{"oper":"grid"},
+                        "prmNames":{"page":"page","rows":"rows","sort":"sidx","order":"sord","search":"_search","nd":"nd","id":"id","filter":"filters","searchField":"searchField","searchOper":"searchOper","searchString":"searchString","oper":"oper","query":"grid","addoper":"add","editoper":"edit","deloper":"del","excel":"excel","subgrid":"subgrid","totalrows":"totalrows","autocomplete":"autocmpl"},
+                        "loadError":function(xhr,status, err){
+                            try {jQuery.jgrid.info_dialog(
+                                     jQuery.jgrid.errors.errcap,'<div class="ui-state-error">'+ xhr.responseText +'</div>',
+                                     jQuery.jgrid.edit.bClose,{buttonalign:'right'}
+                            );}
+                            catch(e) { alert(xhr.responseText);}
+                        },
+                        "pager":"#pager2",
+                        caption:"s"
+                    });
+                    $("#list2").jqGrid('navGrid','#pager2',{del:false,add:false,edit:false},{},{},{},{multipleSearch:true});
+                }
+
+
     </script>
 </head>
 
@@ -61,9 +170,9 @@
 <table id="list2"></table>
 <div id="pager2"></div>
 <script>
-    console.log('a');
-A();
-    console.log('b');
+    $(document).ready(function() {
+AAA();
+    });
 </script>
 
 <h3> There it went – I hope that you saw it</h3>
